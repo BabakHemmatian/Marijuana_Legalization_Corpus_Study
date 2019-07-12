8c7cae6006480a79b51c867c55e5b3d05e2ac202
Babak Hemmatian <babak_hemmatian@brown.edu>
2019-07-12 18:39:21 -0400

    Notes abt changes to parser, fn for lang filtering

--- Reddit_LDA_Analysis.py
+++ Reddit_LDA_Analysis.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 ### import the required modules and functions
 from math import ceil
 import numpy as np
@@ -21,12 +19,31 @@ Write_Performance()
 ### call the parsing function
 
 # NOTE: If NN = False, will pre-process data for LDA.
-# NOTE: If write_original = True, the original text of a relevant comment - without preprocessing - will be saved to a separate file
-# NOTE: If clean_raw = True, the compressed data files will be removed from disk after processing
-# NOTE: Relevance filters can be changed from Utils.py. Do not forget to change the Parser function accordingly
+# NOTE: If write_original = True, the original text of a relevant comment -
+# without preprocessing - will be saved to a separate file
+# NOTE: If clean_raw = True, the compressed data files will be removed from disk
+# after processing
+# NOTE: Relevance filters can be changed from defaults.py
+# NOTE: If there is partial record on file, e.g. including some months in the
+# desired range, but not all, delete the aggregated text files resulting from
+# previous parsing manually before running the function again.These files can be
+# identified by not having a certain month in their filenames and depending on
+# parameter settings can include: author, sentiments, lda_prep, nn_prep,
+# original_comm, original_indices, Random_Count_Dict, Random_Count_List,
+# random_indices, RC_Count_Dict, RC_Count_List, total_count and votes
 
 theparser=Parser()
 theparser.Parse_Rel_RC_Comments()
+
+# Filter the dataset based on whether posts are in English (uses Google's
+# language detection)
+# NOTE: Requires original text of comments to be available on disk
+# NOTE: Should be separately run for LDA and NN, as their preprocessed comments
+# are stored in separate files
+# NOTE: Performance is significantly worse for shorter posts. By default,
+# the filtering is only performed on posts that contain at least 20 words
+theparser.lang_filtering()
+# TODO: Run the function for alternative sentiment estimates after this
 
 ## call the function for calculating the percentage of relevant comments
 if calculate_perc_rel:
@@ -43,7 +60,8 @@ ldam=LDAModel()
 ldam.Define_Sets()
 
 ## read the data and create the vocabulary and the term-document matrix
-# NOTE: Needs loaded sets. Use Define_Sets() before running this function even if prepared sets exist on file
+# NOTE: Needs loaded sets. Use Define_Sets() before running this function even
+# if prepared sets exist on file
 ldam.LDA_Corpus_Processing()
 
 ### Train and Test the LDA Model ###
@@ -52,19 +70,23 @@ ldam.get_model()
 ### calculate a lower bound on per-word perplexity for training and evaluation sets
 
 # NOTE: This function writes the estimates after calculation to the file "perf"
-# NOTE: This is a slow, serial function with no method for looking for previous estimates. Check the disk manually and comment out if estimates already exist
+# NOTE: This is a slow, serial function with no method for looking for previous
+# estimates. Check the file named Performance in the output folder manually
+# and comment out if estimates already exist
 
 if calculate_perplexity:
     train_per_word_perplex,eval_per_word_perplex = ldam.Get_Perplexity()
 
-### calculate umass coherence to allow for interpretability comparison between models with different [num_topics]
+### calculate umass coherence to allow for interpretability comparison between
+# models with different [num_topics]
 
 if calculate_coherence:
     ldam.Get_Coherence()
 
 ### Determine Top Topics Based on Contribution to the Model ###
 
-# NOTE: There is a strict dependency hierarchy between the functions that come in this section and the next. They should be run in the order presented
+# NOTE: There is a strict dependency hierarchy between the functions that come
+# in this section and the next. They should be run in the order presented
 
 ### go through the corpus and calculate the contribution of each topic to comment content in each year
 
@@ -81,12 +103,14 @@ ldam.Get_Topic_Contribution()
 
 ldam.get_top_topics()
 
-## Plot the temporal trends in the top topics
-# NOTE: The resulting figure needs to be closed before functions after this point are run
-# ldam.Plotter(ldam.output_path+'/Temporal_Trend.png')
+## Plot the temporal trends in the top topics and save it to the output path
+ldam.Plotter("{}/Temporal_Trend-{}-{}-{}.png".format(ldam.output_path,
+str(one_hot_topic_contributions),str(num_topics),"idf" if topic_idf else "f"))
 
 ## Find the top words associated with top topics and write them to file
-with open(output_path+'/top_words','a+') as f: # create a file for storing the high-probability words
+with open("{}/top_words-{}".format(
+output_path,"idf" if topic_idf else "f"),'w') as f: # create a file for storing
+# the high-probability words
     for topic in ldam.top_topics:
         print(topic,file=f)
         output = ldam.ldamodel.show_topic(topic,topn=topn)
@@ -94,20 +118,28 @@ with open(output_path+'/top_words','a+') as f: # create a file for storing the h
 
 ### Find the most Representative Comments for the Top Topics ###
 ### Retrieve the probability assigned to top topics for comments in the dataset
-# NOTE: This function only outputs the probabilities for comments of length at least [min_comm_length] with non-zero probability assigned to at least one top topic
+# NOTE: This function only outputs the probabilities for comments of length at
+# least [min_comm_length] with non-zero probability assigned to at least one
+# top topic
 ldam.Get_Top_Topic_Theta()
 
 ## call the function for sampling the most impactful comments
 if num_pop != None:
     ldam.sample_pop()
 
-### for the top topics, choose the [sample_comments] comments that reflect the greatest contribution of those topics and write them to file
-# NOTE: If write_original was set to False during the initial parsing, this function will require the original compressed data files (and will be much slower). If not in the same directory as this file, change the "path" argument
+### for the top topics, choose the [sample_comments] comments that reflect the
+# greatest contribution of those topics and write them to file
+# NOTE: If write_original was set to False during the initial parsing, this
+# function will require all the original compressed data files (and will be much
+# slower). If not in the same directory as this file, change the "path" argument
+# NOTE: This function ignores comments in the incels subreddit, as no vote info
+# for this banned subreddit exists in the source dataset
 ldam.Get_Top_Comments()
 
 ## find top words associated with EVERY topic and write them to file
 top_words_all = {key:[] for key in range(num_topics)}
-with open(output_path+'/top_words_all_'+str(num_topics),'a+') as f: # create a file for storing the high-probability words
+with open(output_path+'/top_words_all_'+str(num_topics),'a+') as f:
+# create a file for storing the high-probability words
     for topic in top_words_all.keys():
         print(topic,file=f)
         output = ldam.ldamodel.show_topic(topic,topn=topn)
