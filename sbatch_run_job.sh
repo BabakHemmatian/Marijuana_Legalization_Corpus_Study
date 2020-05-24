a7f75e3289dfcfd84276248f6be5581e21855d26
Nathaniel Goodman <ngoodma3@node473.oscar.ccv.brown.edu>
2020-05-24 16:03:19 -0400

    Resolving memory issies

--- sbatch_run_job.sh
+++ sbatch_run_job.sh
@@ -5,8 +5,6 @@
 # job name:
 #SBATCH -J Combined_NN
 
-# priority
-#SBATCH --account=ssloman
 
 # email error reports
 #SBATCH --mail-user=nathaniel_goodman@brown.edu 
@@ -20,15 +18,11 @@
 # Request runtime, memory, cores
 #SBATCH --time=24:00:00
 #SBATCH --mem=24G
-#SBATCH -c 14
+#SBATCH -c 12
 #SBATCH -N 1
-#SBATCH --constraint='quadrortx'
-##SBATCH --constraint='cascade'
-#SBATCH -p gpu --gres=gpu:1
-#SBATCH --array=0-3
+
 module load anaconda/3-5.2.0
 conda activate marijuana_study
-##module load python/3.7.4 cuda/10.0.130 cudnn/7.4 tensorflow/2.0.0_gpu_py37
 
 machine='ccv'
 
