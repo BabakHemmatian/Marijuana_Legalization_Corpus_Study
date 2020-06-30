16fff2a94f2cf885df0bbaff41a8047b6c6063d2
nathanielgoodman <nathaniel_goodman@brown.edu>
2020-06-29 21:44:58 -0400

    More files

--- Parse.py
+++ Parse.py
@@ -1,8 +1,8 @@
-#BUG: Need to deal with the case where after CoreNLP fails, there is no sentence
+# BUG: Need to deal with the case where after CoreNLP fails, there is no sentence
 # left.
-#TODO: ignored sentences should also be accounted for. That's different from
+# TODO: ignored sentences should also be accounted for. That's different from
 # corenlp crashing and seems to happen more often
-#TODO: increase memory request: some ignored sentences are rather short
+# TODO: increase memory request: some ignored sentences are rather short
 # Delete the last three months on CCV
 
 # QUESTION: How to determine the number of batches?
@@ -16,18 +16,19 @@ import time
 import sys
 from pycorenlp import StanfordCoreNLP
 from reddit_parser import Parser
+from defaults import num_process
 
 ### call the parsing function
-
 # CoreNLP
 # create a connection to the CoreNLP server to retrieve sentiment
 # (requires CoreNLP_server.py in the same directory)
-subprocess.Popen(['java -mx6g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer --quiet'],
-               shell=True, cwd="./stanford-corenlp-4.0.0")
+subprocess.Popen(
+    ['java -mx6g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -threads ' + str(num_process) + ' --quite'],
+    shell=True, cwd="./stanford-corenlp-4.0.0")
 time.sleep(5)  # wait for connection to the server to be established
 
 # QUESTION: Does the ID need to show up here in the functions too?
-theparser=Parser()
+theparser = Parser()
 
 # Create relevant folders
 theparser.safe_dir_create()
