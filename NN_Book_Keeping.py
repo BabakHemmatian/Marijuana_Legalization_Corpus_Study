927cc80b7601faf7cb5584d50584c3fd3fc85d8b
Babak Hemmatian <babak_hemmatian@brown.edu>
2020-06-30 16:59:08 -0400

    Update NN_Book_Keeping.py

--- NN_Book_Keeping.py
+++ NN_Book_Keeping.py
@@ -1,6 +1,8 @@
-#TODO: Is this the correct Write_Performance()? --> NO, it's not. We should
+# TODO: Is this the correct Write_Performance()? --> NO, it's not. We should
 # update it with the parameters important for the NN in Utils.py
 # Same for NN_param_typecheck in NN_Utils.py
+
+# NOTE: This file should be run only once, before Combined_NN_Model.py
 
 import subprocess
 import time
@@ -12,18 +14,20 @@ from transformers import BertTokenizer
 from NN_Utils import *
 from reddit_parser import Parser # Does the parser object need to be adjusted?
 
-# BUG: parser should be defined\
-# NOTE: should be run only once, before Combined_NN_Model.py
+theparser=Parser()
 
-# if Neural_Relevance_Filtering:
-    # # Needs results from Neural_Relevance_Screen
-    # theparser.Neural_Relevance_Clean()
-    # # TODO: Clean and eval_relevance should be just run locally. Just do those
-    # # if the batch is len(inputs)
+# parse the documents
+theparser.Parse_Rel_RC_Comments()
 
-# NOTE: Requires a Neural_Relevance_Screen random sample hand-annotated for accuracy
-# if eval_relevance:
-#     theparser.eval_relevance()
+if Neural_Relevance_Filtering:
+    # Needs results from Neural_Relevance_Screen
+    theparser.Neural_Relevance_Clean()
+    # TODO: Clean and eval_relevance should be just run locally. Just do those
+    # if the batch is len(inputs)
+
+NOTE: Requires a Neural_Relevance_Screen random sample hand-annotated for accuracy
+if eval_relevance:
+    theparser.eval_relevance()
 
 # NOTE: Don't forget to set NN=True in defaults.py before running this file
 
@@ -31,6 +35,9 @@ from reddit_parser import Parser # Does the parser object need to be adjusted?
 # same file after analyses are performed
 
 Write_Performance()
+
+if add_sentiment:
+    theparser.add_sentiment()
 
 ### call the function for calculating the percentage of relevant comments
 # NOTE: May work only for full-year sets of dates
