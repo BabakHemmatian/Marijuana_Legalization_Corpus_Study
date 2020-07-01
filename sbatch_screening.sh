ebd87aa82be8ef055ccd1c86b14b5f13498f3048
Babak Hemmatian <babak_hemmatian@brown.edu>
2020-07-01 10:35:35 -0400

    Adjust params for GPU run

--- sbatch_screening.sh
+++ sbatch_screening.sh
@@ -7,7 +7,7 @@
 
 
 # email error reports
-#SBATCH --mail-user=nathaniel_goodman@brown.edu 
+#SBATCH --mail-user=babak_hemmatian@brown.edu 
 #SBATCH --mail-type=ALL
 
 # output file
@@ -19,15 +19,18 @@
 
 
 # Request runtime, memory, cores
-#SBATCH --time=48:00:00
-#SBATCH --mem=64G
-#SBATCH -c 32
+#SBATCH --time=6:00:00
+#SBATCH --mem=15G
+#SBATCH -c 2
 #SBATCH -N 1
-#SBATCH --batch_id=0-100
+#SBATCH -p gpu --gres=gpu:1
+#SBATCH --array=0-15
+
+machine='ccv'
 
 eval "$(conda shell.bash hook)"
 conda activate marijuana_study
 
 machine='ccv'
 
-python Neural_Filtering.py --machine $machine 
+python Neural_Filtering.py --machine $machine --$SLURM_ARRAY_TASK_ID
