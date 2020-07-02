648a904e6fa4d6d71306ca1c592fec156addf60e
Babak Hemmatian <babak_hemmatian@brown.edu>
2020-07-01 21:40:47 -0400

    Minor bug fix and additional info

--- NN_Book_Keeping.py
+++ NN_Book_Keeping.py
@@ -2,7 +2,9 @@
 # update it with the parameters important for the NN in Utils.py
 # Same for NN_param_typecheck in NN_Utils.py
 
-# NOTE: This file should be run only once, before Combined_NN_Model.py
+# NOTE: This file should be run only once, before Combined_NN_Model.py and after
+# Parse.py and Neural_Filtering.py are run for the entire dataset, repeatedly if
+# necessary
 
 import subprocess
 import time
@@ -19,15 +21,15 @@ theparser=Parser()
 # parse the documents
 theparser.Parse_Rel_RC_Comments()
 
+# NOTE: Requires a Neural_Relevance_Screen random sample hand-annotated for accuracy
+if eval_relevance:
+    theparser.eval_relevance()
+
 if Neural_Relevance_Filtering:
-    # Needs results from Neural_Relevance_Screen
+    # Needs results from Neural_Relevance_Screen via Neural_Filtering.py
     theparser.Neural_Relevance_Clean()
     # TODO: Clean and eval_relevance should be just run locally. Just do those
     # if the batch is len(inputs)
-
-NOTE: Requires a Neural_Relevance_Screen random sample hand-annotated for accuracy
-if eval_relevance:
-    theparser.eval_relevance()
 
 # NOTE: Don't forget to set NN=True in defaults.py before running this file
 
@@ -37,7 +39,7 @@ if eval_relevance:
 Write_Performance()
 
 if add_sentiment:
-    
+
     # CoreNLP
     # create a connection to the CoreNLP server to retrieve sentiment
     # (requires CoreNLP_server.py in the same directory)
@@ -45,8 +47,8 @@ if add_sentiment:
         ['java -mx6g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -threads ' + str(num_process) + ' --quite'],
         shell=True, cwd="./stanford-corenlp-4.0.0")
     time.sleep(5)  # wait for connection to the server to be established
-    
-    theparser.add_sentiment()
+
+    theparser.add_c_sentiment()
 
 ### call the function for calculating the percentage of relevant comments
 # NOTE: May work only for full-year sets of dates
