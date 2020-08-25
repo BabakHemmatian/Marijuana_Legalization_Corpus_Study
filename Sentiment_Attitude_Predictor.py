af26c314cc59827bdd5381e6eb6815a8265b74ee
nathanielgoodman <nathaniel_goodman@brown.edu>
2020-08-25 09:53:17 -0700

    Temporary changes

--- Sentiment_Attitude_Predictor.py
+++ Sentiment_Attitude_Predictor.py
@@ -8,16 +8,17 @@ with open("original_comm/original_comm") as f:
     original_comm = f.readlines()
 with open("avg_textblob_vader/tv_sentiments") as sents:
     avg_sentiments = sents.readlines()
-print(len(avg_sentiments))
-
 for path in contents:
     df = pd.read_csv(path)
     df["Original Comment Index"] = ""
     df["Average Sentiment"] = ""
-    df.to_csv("sample.csv", index=False)
+    print(path)
     for i, row in df.iterrows():
-        comment = row.text
-        index = original_comm.index(comment)
-        df.at[i, 'Original Comment Index'] = index
-        df.at[i, 'Average Sentiment'] = avg_sentiments[index]
+        comment = row['text']
+        try:
+            index = original_comm.index(comment)
+            df.at[i, 'Original Comment Index'] = index
+            df.at[i, 'Average Sentiment'] = avg_sentiments[index]
+        except ValueError:
+            pass
     df.to_csv(path)
--- sentiment_pretraining/run_model.py
+++ sentiment_pretraining/run_model.py
@@ -1,26 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sqlite3
 import pandas as pd
-import pdb; pdb.set_trace()
+
+# Make Connection to Sqlite DB
+conn = sqlite3.connect('reddit.db')
+cursor = conn.cursor()
+
 
 # Prepare data sets
-from sentiment_pretraining.DataPrep import DataPrep
-from sentiment_pretraining.Model import Model
 
-prep = DataPrep()
-prep.prep_data()
 
+# Function to transform the continuous sentiment rating into a
+# discrete label
+def transform_avg_sentiment(original_sentiment):
+    num = float(original_sentiment)
+    # Negative
+    if num < -0.1:
+        return 0
+    else:
+        # Neutral
+        if -0.1 <= num <= 0.1:
+            return 1
+        # Positive
+        else:
+            return 2
+
+
+def get_comment_sentiment_df():
+    SQL_Query = pd.read_sql_query(
+        '''select 
+            original_comm,
+            t_sentiments,
+            v_sentiments
+            from comments
+        ''', conn)
+    original_df = pd.DataFrame(SQL_Query, columns=['original_comm', 't_sentiments',
+                                                   'v_sentiments'])
+    original_df['avg_sentiment'] = (np.add(original_df['t_sentiments'].split(","),
+                                           original_df['v_sentiments'].split(","))) \
+                                   / len(original_df['v_sentiments'].split(","))
+    return original_df
+
+
+def create_hist_from_data():
+    sentiments_list = get_comment_sentiment_df()['avg_sentiment'].to_list()
+    plt.hist(sentiments_list)
+    plt.show()
+
+
+
+create_hist_from_data()
 # Prepare data frames for training
 
-#train_df = pd.read_csv('train.csv', dtype={'labels': 'int64'})
+# train_df = pd.read_csv('train.csv', dtype={'labels': 'int64'})
 
-#eval_df = pd.read_csv('test.csv', dtype={'labels': 'int64'})
+# eval_df = pd.read_csv('test.csv', dtype={'labels': 'int64'})
 
 
 # Run model
-#model = Model(train_df, eval_df)
+# model = Model(train_df, eval_df)
 
-#model.train()
+# model.train()
 
 # Evaluate model
-#result, model_outputs, wrong_predictions = model.eval()
-
-
+# result, model_outputs, wrong_predictions = model.eval()
