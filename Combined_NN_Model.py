871d02909b8928323e1b1fd37123ac55d86a4cf5
Babak Hemmatian <babak_hemmatian@brown.edu>
2020-09-07 22:16:50 -0400

    Update Combined_NN_Model.py

--- Combined_NN_Model.py
+++ Combined_NN_Model.py
@@ -1,13 +1,10 @@
 ### import the required modules and functions
 
-import subprocess
 import time
 import sys
-from Utils import Write_Performance
+from Utils import *
 from config import *
 from ModelEstimation import NNModel
-from transformers import BertTokenizer
-from NN_Utils import *
 from reddit_parser import Parser # Does the parser object need to be adjusted?
 
 # QUESTION: Does the ID need to show up here in the functions too?
@@ -19,14 +16,11 @@ theparser.safe_dir_create()
 # parse the documents
 theparser.Parse_Rel_RC_Comments()
 
+### check key hyperparameters for correct data types
+NN_param_typecheck()
 
 ### Define the neural network object
-
 nnmodel=NNModel()
-
-### check key hyperparameters for correct data types
-
-nnmodel.NN_param_typecheck()
 
 ### create training, development and test sets
 
@@ -35,7 +29,6 @@ nnmodel.NN_param_typecheck()
 # should not be changed between the creation of sets for LDA and NN analyses
 
 ## Determine the comments that will comprise various sets
-
 nnmodel.Define_Sets()
 
 ## Read and index the content of comments in each set
