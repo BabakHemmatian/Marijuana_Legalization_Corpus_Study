9b5a4a5d022c51036f28ce2b0321dae5f423c36e
Babak Hemmatian <babak_hemmatian@brown.edu>
2020-09-11 11:45:30 -0400

    Update Combined_NN_Model.py

--- Combined_NN_Model.py
+++ Combined_NN_Model.py
@@ -31,13 +31,13 @@ nnmodel=NNModel()
 ## Determine the comments that will comprise various sets
 # NOTE: For DOI training, feed in [human_ratings_path] as an argument. The
 # filename format will be inferred based on parameters in defaults.py
-# TODO: fix the path variable. We might want to move it.
+# TODO: fix the path variable. We might want to move the rating files
+# TODO: Debug the function, maybe add the indices to the SQL database.
 nnmodel.Define_Sets(human_ratings_path = model_path + "/auto_labels")
 
 ## Read and index the content of comments in each set
-
-for set_key in nnmodel.set_key_list:
-    nnmodel.Index_Set(set_key)
+# TODO: Set this up to add the RoBERTa activations as flattened column to the database
+nnmodel.RoBERTa_Set()
 
 #TODO: Add the getting sentiments and human ratings as part of the training func,
 # wuth path arguments if needed
@@ -61,3 +61,6 @@ nnmodel.Get_Set_Lengths()
 # in defaults.py
 
 nnmodel.train_and_evaluate()
+
+# TODO: implement
+nnmodel.test()
