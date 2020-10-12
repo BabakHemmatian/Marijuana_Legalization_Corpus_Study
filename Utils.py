70dee266af80c76bcadb32b87adc92b23c4e83e5
Babak Hemmatian <babak_hemmatian@brown.edu>
2020-10-12 13:35:37 -0400

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -5,8 +5,30 @@ import time
 from config import *
 import reddit_parser
 
+def NN_param_typecheck():
+    assert type(DOI) is str
+    assert type(RoBERTa_model) is str
+    assert type(pretrained) is bool
+    assert 0 < NN_training_fraction and 1 >= NN_training_fraction
+    assert type(FrequencyFilter) is int
+    assert type(epochs) is int
+    if type(learning_rate) is int or type(learning_rate) is float:
+        assert 0 < learning_rate and 1 > learning_rate
+    elif type(learning_rate) is list:
+        for rate in learning_rate:
+            assert 0 < learning_rate and 1 > learning_rate
+    else:
+        raise Exception("Learning rate format not recognized.")
+    assert type(batchSz) is int
+    assert type(hiddenSz) is int
+    assert type(ff2Sz) is int
+    # assert 0 < keepP and 1 >= keepP
+    # assert type(l2regularization) is bool
+    # if l2regularization == True:
+    #     assert 0 < alpha and 1 > alpha
+    assert type(early_stopping) is bool
+
 ### Function for writing parameters and model performance to file
-## TODO: NN variables should be updated
 def Write_Performance(output_path=output_path, NN=NN):
     with open(output_path+"/Performance",'a+') as perf:
         if not NN:
@@ -33,76 +55,29 @@ def Write_Performance(output_path=output_path, NN=NN):
         else: # if running a neural network analysis
 
             # record the pre-processing paramters
+            print("Entire corpus used = " + str(ENTIRE_CORPUS),file=perf)
             print("Training fraction = " + str(NN_training_fraction),file=perf)
-            print("Vocabulary size = " + str(MaxVocab),file=perf)
             print("Frequency filter = below " + str(FrequencyFilter),file=perf)
 
             # record the kind of network
             print("***",file=perf)
-            print("special_doi = " + str(special_doi),file=perf)
-            print("pretrained = " + str(pretrained),file=perf)
+            print("Dimension of Interest = " + str(DOI),file=perf)
+            print("RoBERTa model version = {}".format(RoBERTa_model),file=perf)
+            print("Sentiment pretraining = " + str(pretrained),file=perf)
+            if LDA_topics:
+                print("Number of LDA topics used = " + str(num_topics),file=perf)
+            if authorship:
+                print("Authors with more than {} comments parametrized.".format(min_authorship),file=perf)
 
             # record the hyperparameters
             print("Number of epochs: "+str(epochs),file=perf)
             print("Learning_rate = " + str(learning_rate),file=perf)
-            print("Batch size = " + str(batchSz),file=perf)
-            ##TODO: should this be author_embedSz or word_embedSz?
-            print("Embedding size = " + str(word_embedSz),file=perf)
-            print("Recurrent layer size = " + str(hiddenSz),file=perf)
-            print("1st feedforward layer size = " + str(ff1Sz),file=perf)
+            print("Batch size = " + str(batch_size),file=perf)
             print("2nd feedforward layer size = " + str(ff2Sz),file=perf)
-            print("Dropout rate = " + str(1 - keepP),file=perf)
-            print("L2 regularization = " + str(l2regularization),file=perf)
-            print("L2 regularization constant = " + str(alpha),file=perf)
             print("Early stopping = " + str(early_stopping),file=perf)
-
-### calculate the yearly relevant comment counts
-def Get_Counts(path=path, random=False, frequency="monthly"):
-    assert frequency in ("monthly", "yearly")
-
-    fns=reddit_parser.Parser().get_parser_fns()
-    fn=fns["counts"] if not random else fns["counts_random"]
-    # check for monthly relevant comment counts
-    if not Path(fn).is_file():
-        raise Exception('The cummulative monthly counts could not be found')
-
-    # load monthly relevant comment counts
-    with open(fn,'r') as f:
-        timelist = []
-        for line in f:
-            if line.strip() != "":
-                timelist.append(int(line))
-
-    # intialize lists and counters
-    cumulative = [] # cummulative number of comments per interval
-    per = [] # number of comments per interval
-
-    month_counter = 0
-
-    # iterate through monthly counts
-    for index,number in enumerate(timelist): # for each month
-        month_counter += 1 # update counter
-        if frequency=="monthly":
-            cumulative.append(number) # add the cummulative count
-            if index == 0: # for the first month
-                per.append(number) # append the cummulative value to number of comments per year
-            else: # for the other months, subtract the last two cummulative values to find the number of relevant comments in that year
-                per.append(number - cumulative[-2])
-
-        else:
-            if (month_counter % 12) == 0 or index == len(timelist) - 1: # if at the end of the year or the corpus
-                cumulative.append(number) # add the cummulative count
-
-                if index + 1 == 12: # for the first year
-                    per.append(number) # append the cummulative value to number of comments per year
-                else: # for the other years, subtract the last two cummulative values to find the number of relevant comments in that year
-                    per.append(number - cumulative[-2])
-                    month_counter = 0 # reset the counter at the end of the year
-
-    assert sum(per) == cumulative[-1], "Monthly counts do not add up to the total count"
-    assert cumulative[-1] == timelist[-1], "Total count does not add up to the number of posts on file"
-
-    return per,cumulative
+            # print("Dropout rate = " + str(1 - keepP),file=perf)
+            # print("L2 regularization = " + str(l2regularization),file=perf)
+            # print("L2 regularization constant = " + str(alpha),file=perf)
 
 def essentially_eq(a, b):
     return abs(a-b)<= 0.1
