8b534ab8a6565aebba3975c3b1708908aeb1f827
Jonathan Lee <jonalee99@gmail.com>
2020-10-19 14:17:25 -0400

    update

--- Jonathan_Scratch.py
+++ Jonathan_Scratch.py
@@ -8,6 +8,53 @@ from sklearn.datasets import load_iris
 from sklearn.cluster import AgglomerativeClustering
 
 
+def main():
+
+    # Load a pretrained model from disk.
+    lda = LdaModel.load("RC_LDA_50_True.lda")
+
+    # Load in the dictionary
+    dictionary = ["Tobacco vs Marijuana vs Alcohol", "UNCLEAR/JUNK", "Growing Weed", "Liberal vs Conservative",
+                  "Quantity/Duration", "UNCLEAR/JUNK", "Government + Systemic Power", "War on Drugs",
+                  "Federal vs State vs Local Government", "Information on Marijuana", "Crime", "Legal Marijuana Market",
+                  "Marijuana as it Relates to Other Drugs", "Legalizing Marijuana", "Existing Marijuana Legalization",
+                  "Marijuana Busting", "Medicinal Marijuana", "Marijuana Plant Contents", "International + Employment",
+                  "FDA Scheduled Drugs", "UNCLEAR/JUNK", "President and International Affairs", "Family Abuse",
+                  "Canada and Housing Prices", "Smoking Marijuana", "Private Industry", "West Coast + Supplements",
+                  "International", "Police", "Legal Case", "UNCLEAR/JUNK", "Reddit", "UNCLEAR/JUNK", "UNCLEAR/JUNK",
+                  "Legal", "Drug Test", "UNCLEAR/JUNK", "Criminal Punishment", "Legalizing Marijuana", "Local delivery",
+                  "Marijuana Regulation", "Marijuana Research", "Driving Drunk/High", "Racial Disparities",
+                  "Court Case", "Smoking Marijuana/Tobacco/E-Cig", "Edible Foods/Mixed", "Law Enforcement",
+                  "Gun Control", "Expletives"]
+
+    # Get the distribution
+    distribution = lda.get_topics()
+
+    # Remove junk indices [1,5,18,20,21,23,26,27,30,31,32,33,36,46]
+    remove_index = np.array([1, 5, 18, 20, 21, 23, 26, 27, 30, 31, 32, 33, 36, 46])
+
+    # track array
+    track_array = np.arange(50)
+
+    # Remove the junk indices
+    for i in range(len(remove_index)):
+        current_index = remove_index[len(remove_index) - i - 1]
+        distribution = np.delete(distribution, current_index, 0)
+        track_array = np.delete(track_array, current_index, 0)
+
+    # setting distance_threshold=0 ensures we compute the full tree.
+    model = AgglomerativeClustering(distance_threshold=0, n_clusters=None, affinity="l1", linkage="complete")
+    model.fit(distribution)
+    plt.title('Hierarchical Clustering Dendrogram')
+
+    # plot the top three levels of the dendrogram
+    plot_dendrogram(model, truncate_mode='level', p=100, leaf_label_func=(lambda id: dictionary[track_array[id]]),
+                    leaf_rotation=90)
+    plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+    plt.tight_layout()
+    plt.show()
+
+
 def plot_dendrogram(model, **kwargs):
     # Create linkage matrix and then plot the dendrogram
 
@@ -29,43 +76,6 @@ def plot_dendrogram(model, **kwargs):
 
     # Plot the corresponding dendrogram
     dendrogram(linkage_matrix, **kwargs)
-
-
-def main():
-
-    # Load a pretrained model from disk.
-    lda = LdaModel.load("RC_LDA_50_True.lda")
-
-    # Load in the dictionary
-    dictionary = Dictionary.load("RC_LDA_Dict_True.dict")
-
-    # Get the distribution
-    distribution = lda.get_topics()
-
-    # Remove junk indices [1,5,18,20,21,23,26,27,30,31,32,33,36,46]
-    remove_index = np.array([1, 5, 18, 20, 21, 23, 26, 27, 30, 31, 32, 33, 36, 46])
-
-    # track array
-    track_array = np.arange(50)
-
-    # Remove the junk indices
-    for i in range(len(remove_index)):
-        current_index = remove_index[len(remove_index) - i - 1]
-        distribution = np.delete(distribution, current_index, 0)
-        track_array = np.delete(track_array, current_index, 0)
-
-    # setting distance_threshold=0 ensures we compute the full tree.
-    model = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
-    model.fit(distribution)
-    plt.title('Hierarchical Clustering Dendrogram')
-
-    # plot the top three levels of the dendrogram
-    print(dictionary[track_array[18]])
-    # plot_dendrogram(model, truncate_mode='level', p=100)
-    plot_dendrogram(model, truncate_mode='level', p=100, leaf_label_func=lambda id: dictionary[track_array[id]], leaf_rotation=90)
-    plt.xlabel("Number of points in node (or index of point if no parenthesis).")
-    plt.tight_layout()
-    plt.show()
 
 
 if __name__ == "__main__":
